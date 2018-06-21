b863e1b993837ceab3da229ed3e63c478a4fc45b
luissantiagodev <luissantiagodev@gmail.com>
2018-06-21 08:30:49 -0500

    bugs fixed

--- api/routes/orderProyector.js
+++ api/routes/orderProyector.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose')
 const OrderProyector = require('../models/orderProyector.js')
 const PORT = process.env.PORT || 8000;
 const Proyector = require('../models/Proyector.js')
+const moment = require('moment')
+const path = require('path')
+const pdfMakePrinter = require('pdfmake')
+const fs = require('fs')
 
 router.get('/', (req, res) => {
     OrderProyector
@@ -151,6 +155,146 @@ router.delete('/:id', (req, res) => {
             })
         })
 })
+
+
+router.get('/reporte/:start/:end' , (req , res)=>{
+    let startDateReq = req.params.start
+    let endDateReq = req.params.end
+    OrderProyector
+        .find()
+        .select('proyector teacher date initialHour finalHour place')
+        .populate('proyector teacher')
+        .exec()
+        .then(docs => {
+            let originalArray = docs.map(doc=>{
+                return{
+                    id : doc.id,
+                    Proyector : doc.proyector.name,
+                    Nombre : doc.teacher.name,
+                    Fecha : moment(doc.date).format('L'),
+                    Hora_Inicial : moment(doc.initialHour).format('HH:mm:ss'),
+                    Hora_Final : moment(doc.finalHour).format('HH:mm:ss'),
+                    lugar : doc.place,
+                }
+            })
+
+            let newArray = originalArray.filter(x=>{
+                var compareDate = moment(x.date);
+                var startDate   = moment(startDateReq);
+                var endDate     = moment(endDateReq);
+                if(compareDate.isBetween(startDate, endDate)){
+                    return true;
+                }else{
+                    return false;
+                }
+            })
+
+            console.log(newArray)
+            var dd = {
+                content: [
+                    table(newArray, ['Proyector', 'Nombre' , "Hora_Inicial" , "Hora_Final" , "Fecha" , "lugar"])
+                ],
+                header:  {
+                    margin: 10,
+                    columns: [
+                        {
+                            margin: [10, 10, 0, 0],
+                            text: 'Reporte de proyectores'
+                        }
+                    ]
+                },
+                styles: {
+                    header: {
+                        fontSize: 18,
+                        bold: true
+                    },
+                      keepmargin: {
+                alignment: 'center',
+                margin: [0,300,0,0]
+            },
+                }
+            }
+            createPdfBinary(dd, res)
+            //res.status(200).json(newArray)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+
+
+function buildTableBody(data, columns) {
+    var body = [];
+
+    body.push(columns);
+
+    data.forEach(function (row) {
+        console.log(row)
+        var dataRow = [];
+        columns.forEach(function (column) {
+            //console.log(column)
+            dataRow.push(row[column].toString());
+        })
+
+        body.push(dataRow);
+    });
+
+    return body;
+}
+
+function table(data, columns) {
+    return {
+        table: {
+            layout: 'lightHorizontalLines', // optional
+            headerRows: 1,
+            widths: ['auto', 'auto', 'auto', 'auto','auto', '*'],
+            margin: [0 , 700 , 0 ,0],
+            body: buildTableBody(data, columns)
+        }
+    };
+}
+
+
+function createPdfBinary(pdfDoc, res) {
+    var temp123;
+
+    const fontDescriptors = {
+        Roboto: {
+            normal: path.join(__dirname + '/Roboto/Roboto-Regular.ttf'),
+            italics: path.join(__dirname + '/Roboto/Roboto-Italic.ttf'),
+            bold: path.join(__dirname + '/Roboto/Roboto-Medium.ttf'),
+            bolditalics: path.join(__dirname + '/Roboto/Roboto-MediumItalic.ttf')
+        }
+    };
+
+    var printer = new pdfMakePrinter(fontDescriptors);
+    var doc = printer.createPdfKitDocument(pdfDoc);
+    var chunks = [];
+    var result;
+
+    doc.on('data', (chunk) => {
+        chunks.push(chunk);
+    });
+
+
+    doc.pipe(temp123 = fs.createWriteStream(__dirname + "/example.pdf"), {
+        encoding: 'utf16'
+    });
+
+
+    temp123.on('finish', async function () {
+        // do send PDF file 
+        res.status(200).json({
+            nameOfFile : "/example.pdf"
+        })
+    });
+
+    doc.end();
+}
+
 
 
 module.exports = router
